46fe904dd41a384d272f58bb410bff7b70460c2a
Keluarga Senyum <118404518+keluargasenyum@users.noreply.github.com>
2022-11-17 10:06:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.write("""
 # Keluarga Senyum
 """)
-a = 2
-b = 3
-t = a+b
-st.write("Nilai :",t)
+a = str(input("Nama : "))
+b = str(input("Kelas : "))
+st.write("Nama : ",a)
+st.write("Kelas : ",b)

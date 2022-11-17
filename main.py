249f8c9ea2ed86b25233dfcda4e2de4d48a60314
Keluarga Senyum <118404518+keluargasenyum@users.noreply.github.com>
2022-11-17 10:22:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import streamlit as st
 st.write("""
 # Keluarga Senyum
 """)
+menu = {
+"Ayam goreng" : 20000,
+"Asin " : 60000
+}
+
+for i in menu
+   st.write("Menu",i,"Harga",menu[i]
+
+
+
+
 a = st.text_input("Nama : ")
 b = st.text_input("Kelas : ")
 st.write("Nama : ",a)

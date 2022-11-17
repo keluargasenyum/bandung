827439205e2b0d3050eb373e62ca15f2fec3a38b
Keluarga Senyum <118404518+keluargasenyum@users.noreply.github.com>
2022-11-17 09:59:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ st.write("""
 # Keluarga Senyum
 """)
 
-number = st.txt_input("Nama : ")
+st.select_slider(label, options=(), value=None, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
